6daaf439ead515737cdc097f601715e0f18eba09
PlugaruT <plugaru_tudor@hotmail.com>
2015-02-15 20:49:55 +0200

    little update

--- data/my-script.js
+++ data/my-script.js
@@ -20,12 +20,23 @@ function CheckSite(){
 function ShowBar(type_site)
 {
 	console.log(type_site);
-	var message = "WARNING! Please close the following website, may affect your brain! :D";
+	var message = "WARNING! Please close the website, may affect your brain! :D";
 	var clas = "danger";
+	var ico = ( "<i class= 'fa fa-plus-circle'></i>" );
 	if (type_site) {
 		message = "GOOD JOB! This website is good!";
 		clas = "success";
+		var ico = ( "<i class= 'fa fa-check'></i>" );
 	};
-	var newdiv1 = ( "<div class='alert alert-" + clas + "' role='alert' style='text-align:center;'> " + message + "</div>");
+	var icon_class = "fa fa-times";
+	var icon_style = "font-size:20px; top: 0px; position:absolute; right:0px; cursor: pointer;"
+	var icon = ( "<i class= 'fa fa-times' id = 'lala' style = '" + icon_style +"' ></i>" );
+	var newdiv1 = ( "<div class='alert alert-" + clas + "' role='alert' style='text-align:center;display:none;' id='mediapoint'> " + ico + message + icon + "</div>");
+	$( "#mediapoint" ).remove();
 	$( "body" ).prepend(newdiv1);
+	//$( "body" ).prepend(icon);
+	$( "#mediapoint" ).show('fast');
+	$( "#lala" ).on('click', function() {
+		$( "#mediapoint" ).hide('slow');
+	});
 }
